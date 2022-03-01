8a551def94ca5f249a1b4ee11f36e0bae122dc0d
JenniferMrm <jennifer-miramon_student2021@wilder.school>
2022-03-01 10:54:42 +0100

    :sparkles: [feat] : Add navbar components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React from "react";
 import "@/css/style.scss";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Main from "@layout/Main/Main";
-import Home from "@view/Home/Home";
+import Modal from "@components/global/Modal";
+// import Main from "@layout/Main/Main";
+// import Home from "@view/Home/Home";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route
+        {/* <Route
           path="/"
           element={
             <Main>
               <Home />
             </Main>
           }
-        />
+        /> */}
+        <Route path="/" element={<Modal />} />
       </Routes>
     </Router>
   );
