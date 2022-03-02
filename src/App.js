b3a1f50949c37c239b68daa30d88984ec0752adc
JenniferMrm <jennifer-miramon_student2021@wilder.school>
2022-03-02 12:44:42 +0100

    :sparkles: [feat] : Add navbar and modal for mobile viewport

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import "@/css/style.scss";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Modal from "@components/global/Modal";
+// import Modal from "@components/global/Modal";
+import Navbar from "./components/global/Navbar";
 // import Main from "@layout/Main/Main";
 // import Home from "@view/Home/Home";
 
@@ -17,7 +18,7 @@ function App() {
             </Main>
           }
         /> */}
-        <Route path="/" element={<Modal />} />
+        <Route path="/" element={<Navbar />} />
       </Routes>
     </Router>
   );
