1926f527fb96b7bede49cd30f84da3fe3634e2df
JenniferMrm <jennifer-miramon_student2021@wilder.school>
2022-03-23 22:29:19 +0100

    :rocket: [deploy] : Update environment variables

--- src/view/NotFound/NotFound.jsx
+++ src/view/NotFound/NotFound.jsx
@@ -7,7 +7,7 @@ function NotFound(props) {
       <h2 className="not-found__code">404 Not Found</h2>
       <h3 className="not-found__oops">Oops,</h3>
       <h5 className="not-found__lost">you got lost !</h5>
-      <img className="not-found__image" src="http://localhost:3000/assets/rescuecopter.webp" alt="Animal Crossing Rescue Helicopter" />
+      <img className="not-found__image" src={process.env.REACT_APP_ACNHDEX_URL} alt="Animal Crossing Rescue Helicopter" />
       <Link to="/" className="not-found__link" role="button">
         <p className="not-found__link__title">Back to Home</p>
       </Link>
