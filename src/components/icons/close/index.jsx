3b3115236a9db39dd0d831a8718e8852c33f546d
JenniferMrm <jennifer-miramon_student2021@wilder.school>
2022-07-13 18:37:58 +0200

    :sparkles: [feat] : Replace all icons

---
+++ src/components/icons/close/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function IconClose(props) {
+  return (
+    <div class="icon">
+      <svg width="42" height="42" viewBox="0 0 42 42" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M10 10L32 32" stroke="black" stroke-width="3" stroke-linecap="round" />
+        <path d="M10 32L33 10" stroke="black" stroke-width="3" stroke-linecap="round" />
+      </svg>
+    </div>
+  );
+}
+
+export default IconClose;
